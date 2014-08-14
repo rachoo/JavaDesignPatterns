4bfad52eebf35e4993326ef2747fd6c1c1b4e254
rachoo <rachoo8906@gmail.com>
2014-08-14 17:07:16 +0800

    add demo files

--- src/cn/com/nianduen/Demo.java
+++ src/cn/com/nianduen/Demo.java
@@ -4,4 +4,9 @@ package cn.com.nianduen;
  * Created by rachoochen on 8/14/14.
  */
 public class Demo {
+
+    public static String getString(){
+        return "hello world";
+    }
+
 }
--- test/cn/com/nianduen/DemoTest.java
+++ test/cn/com/nianduen/DemoTest.java
@@ -10,16 +10,17 @@ public class DemoTest {
 
     @Before
     public void setUp() throws Exception {
-
+        System.out.println("setup");
     }
 
     @After
     public void tearDown() throws Exception {
-
+        System.out.println("tearDown");
     }
 
     @Test
     public void testGetString() throws Exception {
-
+        String str = Demo.getString();
+        System.out.println(str);
     }
 }
