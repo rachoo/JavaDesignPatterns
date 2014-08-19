a9a4c63e240b7226bea8271c2c5ab372a7b7edfc
rachoo <rachoo8906@gmail.com>
2014-08-19 11:19:33 +0800

    add files

--- src/main/java/cn/com/nianduen/strategy/EnumStrategy.java
+++ src/main/java/cn/com/nianduen/strategy/EnumStrategy.java
@@ -1,7 +1,31 @@
 package cn.com.nianduen.strategy;
 
 /**
+ * A enum used strategy pattern
  * Created by rachoochen on 8/19/14.
  */
 public enum EnumStrategy {
+    ADD("+"){
+        public int exec(int a,int b){
+            return a+b;
+        }
+    },
+    SUB("-"){
+        public int exec(int a,int b){
+            return a - b;
+        }
+    };
+
+    private String value;
+
+    EnumStrategy(String value) {
+        this.value = value;
+    }
+
+    public abstract int exec(int a,int b);
+
+    public String getValue() {
+        return value;
+    }
+
 }
--- src/test/java/cn/com/nianduen/strategy/EnumStrategyTest.java
+++ src/test/java/cn/com/nianduen/strategy/EnumStrategyTest.java
@@ -7,12 +7,13 @@ import static org.junit.Assert.*;
 public class EnumStrategyTest {
 
     @Test
-    public void testExec() throws Exception {
+    public void test() throws Exception {
+
+        System.out.println(EnumStrategy.ADD.exec(2, 10));
+        System.out.println(EnumStrategy.ADD.getValue());
+        System.out.println(EnumStrategy.SUB.exec(100, 1000));
+        System.out.println(EnumStrategy.SUB.getValue());
 
     }
 
-    @Test
-    public void testGetValue() throws Exception {
-
-    }
 }
