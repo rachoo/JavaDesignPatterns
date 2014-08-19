a4fadda36f323cc7e36f67a60f1a684c3961a324
rachoo <rachoo8906@gmail.com>
2014-08-19 10:54:01 +0800

    add strategy pattern

---
+++ src/main/java/cn/com/nianduen/strategy/MethodB.java
@@ -0,0 +1,11 @@
+package cn.com.nianduen.strategy;
+
+/**
+ * Created by rachoochen on 8/19/14.
+ */
+public class MethodB implements Istrategy {
+    @Override
+    public void operation() {
+        System.out.println("method b");
+    }
+}
