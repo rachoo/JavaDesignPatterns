3c00f09d0936867f90fa3b43bbd7250039bf9a11
rachoo <rachoo8906@gmail.com>
2014-08-19 11:18:29 +0800

    add strategy enum

---
+++ src/main/java/cn/com/nianduen/strategy/EnumStrategy.java
@@ -0,0 +1,7 @@
+package cn.com.nianduen.strategy;
+
+/**
+ * Created by rachoochen on 8/19/14.
+ */
+public enum EnumStrategy {
+}
---
+++ src/test/java/cn/com/nianduen/strategy/EnumStrategyTest.java
@@ -0,0 +1,18 @@
+package cn.com.nianduen.strategy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EnumStrategyTest {
+
+    @Test
+    public void testExec() throws Exception {
+
+    }
+
+    @Test
+    public void testGetValue() throws Exception {
+
+    }
+}
